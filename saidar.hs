030e5b5d9a7d32f026dbf4773f36cb7eaa60fa23
Tom Wilkinson <email@tswilkinson.me>
2023-04-05 22:20:29 +0100

    Test

--- saidar.hs
+++ saidar.hs
@@ -1,5 +1,8 @@
 main :: IO ()
-main = print "Hello world!"
+main = let identity :: Expression
+           identity = Lambda (Universe Zero) (Lambda (Variable Zero) (Variable Zero))
+--        in print $ simplification_and_type (Lambda (Universe Zero) (Lambda (Variable Zero) (Application (Variable Zero) (Application (Variable (Succ Zero)) identity))))
+        in print $ simplification_and_type (Lambda (Universe Zero) (Lambda (Variable Zero) identity))
 
 data Natural = Zero | Succ Natural
 
@@ -12,6 +15,10 @@ instance Ord Natural where
   Zero <= _ = True
   (Succ _) <= Zero = False
   (Succ n) <= (Succ m) = n <= m
+
+instance Show Natural where
+  show Zero = "0"
+  show (Succ n) = show n ++ "+"
 
 data Expression = Variable Natural
                 | Lambda Expression Expression
@@ -26,6 +33,13 @@ instance Eq Expression where
   (ArrowType t s) == (ArrowType x y) = t == x && s == y
   (Universe n) == (Universe m) = n == m
   _ == _ = False
+
+instance Show Expression where
+  show (Variable n) = "@" ++ show n
+  show (Lambda l r) = "(" ++ show l ++ " |-> " ++ show r ++ ")"
+  show (Application l r) = show l ++ " " ++ show r
+  show (ArrowType t s) = "(" ++ show t ++ " -> " ++ show s ++ ")"
+  show (Universe n) = "U_" ++ show n
 
 simplification_and_type :: Expression -> Maybe (Expression,Expression)
 simplification_and_type = go []
@@ -60,11 +74,26 @@ simplification_and_type = go []
 
           further :: [Expression] -> Natural -> Maybe Expression
           further [] _ = Nothing
-          further (t:_) Zero = Just t
+          further (t:_) Zero = Just (increment t)
           further (_:ts) (Succ n) = further ts n
 
+          increment :: Expression -> Expression
+          increment (Variable n) = Variable (Succ n)
+          increment (Lambda t y) = Lambda (increment t) (increment y)
+          increment (Application x f) = Application (increment x) (increment f)
+          increment (ArrowType l r) = ArrowType (increment l) (increment r)
+          increment (Universe n) = Universe n
+
           apply :: Expression -> Natural -> Expression -> Expression
-          apply x n (Variable m) = if n == m then x else Variable m
+          apply x n (Variable m) = deeper n m
+            where deeper :: Natural -> Natural -> Expression
+                  deeper Zero Zero = x
+                  deeper Zero (Succ b) = Variable b
+                  deeper (Succ _) Zero = Variable Zero
+                  deeper (Succ a) (Succ b) = deeper a b
           apply x n (Lambda l r) = Lambda (apply x n l) (apply x (Succ n) r)
-          apply x n (Application l r) = case apply x (Succ n) r of
-            Lambda t x -> Universe Zero
+          apply x n (Application l r) = case apply x n r of
+            Lambda _ b -> apply (apply x n l) Zero b
+            a -> Application (apply x n l) a
+          apply x n (ArrowType a b) = ArrowType (apply x n a) (apply x n b)
+          apply _ _ (Universe m) = Universe m
